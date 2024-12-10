d9c60c24a8eb5e705be768a543b14725519b18a4
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-12-10 12:49:21 -0300

    feat: refactoring screens and creating icons

--- app/src/main/java/br/com/mizaeldouglas/rideway_app_teste_emprego_shopper/presentation/activities/MainActivity.kt
+++ app/src/main/java/br/com/mizaeldouglas/rideway_app_teste_emprego_shopper/presentation/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package br.com.mizaeldouglas.rideway_app_teste_emprego_shopper.prensentation.activities
+package br.com.mizaeldouglas.rideway_app_teste_emprego_shopper.presentation.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.com.mizaeldouglas.rideway_app_teste_emprego_shopper.R
 
-class TravelHistoryActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_travel_history)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
