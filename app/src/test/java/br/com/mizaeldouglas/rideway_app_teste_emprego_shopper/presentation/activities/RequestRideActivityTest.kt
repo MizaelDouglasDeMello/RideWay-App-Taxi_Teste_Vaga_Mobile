d668ab54bec0ed5fdddb1c11116200f464d30afc
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-12-10 21:52:44 -0300

    feat: created tests

---
+++ app/src/test/java/br/com/mizaeldouglas/rideway_app_teste_emprego_shopper/presentation/activities/RequestRideActivityTest.kt
@@ -0,0 +1,19 @@
+package br.com.mizaeldouglas.rideway_app_teste_emprego_shopper.presentation.activities
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+@ExperimentalCoroutinesApi
+class RequestRideActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
---
+++ app/src/test/java/br/com/mizaeldouglas/rideway_app_teste_emprego_shopper/presentation/viewmodel/RequestRideViewModelTest.kt
@@ -0,0 +1,17 @@
+package br.com.mizaeldouglas.rideway_app_teste_emprego_shopper.presentation.viewmodel
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class RequestRideViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
